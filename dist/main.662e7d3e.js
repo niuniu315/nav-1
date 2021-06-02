a63aefcaf3da34490c2aaadc8f2e3510dc75846a
niuniu315 <454378439@qq.com>
2021-06-02 13:41:13 +0800

    写入了天气

--- dist/main.662e7d3e.js
+++ dist/main.662e7d3e.js
@@ -185,7 +185,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '54521' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '54971' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
